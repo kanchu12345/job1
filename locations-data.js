7369f5b7e5cde0cfd3add4a25d1ba50e254b90ae
kanchu12345 <44029419+kanchu12345@users.noreply.github.com>
2026-05-25 16:00:40 +0530

    up

---
+++ locations-data.js
@@ -0,0 +1,319 @@
+const SL_LOCATIONS = {
+    "Colombo": {
+        "DS_Divisions": {
+            "Colombo": ["Fort", "Slave Island", "Colpetty", "Bambalapitiya", "Havelock Town", "Wellawatte", "Cinnamon Gardens", "Borella", "Dematagoda", "Maradana", "Pettah", "Hultsdorf", "Kotahena", "Grandpass", "Mutwal"],
+            "Dehiwala": ["Dehiwala", "Mount Lavinia", "Attidiya", "Kalubowila", "Kohuwala", "Ratmalana"],
+            "Homagama": ["Homagama", "Kottawa", "Pannipitiya", "Mattegoda", "Diyagama", "Meegoda"],
+            "Kaduwela": ["Kaduwela", "Malabe", "Battaramulla", "Koswatta", "Thalahena", "Rajagiriya", "Piliyandala"],
+            "Kesbewa": ["Kesbewa", "Piliyandala", "Kahathuduwa", "Madapatha", "Bokundara"],
+            "Kolonnawa": ["Kolonnawa", "Wellampitiya", "Gotatuwa"],
+            "Maharagama": ["Maharagama", "Boralesgamuwa", "Nugegoda", "Mirihana", "Navinna", "Puwakpitiya"],
+            "Moratuwa": ["Moratuwa", "Katubedda", "Rawathawatte", "Egodauyana"],
+            "Padukka": ["Padukka", "Meepe", "Galagedera"],
+            "Ratmalana": ["Ratmalana", "Mount Lavinia", "Kandawala"],
+            "Seethawaka": ["Avissawella", "Puwakpitiya", "Kosgama"],
+            "Sri Jayawardenepura Kotte": ["Nugegoda", "Rajagiriya", "Kotte", "Nawala", "Ethul Kotte", "Mirihana"],
+            "Hanwella": ["Hanwella", "Jathika", "Ranala"]
+        }
+    },
+    "Gampaha": {
+        "DS_Divisions": {
+            "Attanagalla": ["Veyangoda", "Nittambuwa", "Pasyala"],
+            "Biyagama": ["Biyagama", "Malwana", "Mawaramandiya"],
+            "Divulapitiya": ["Divulapitiya", "Badalgama", "Dunagaha"],
+            "Dompe": ["Dompe", "Kirindiwela", "Pugoda"],
+            "Gampaha": ["Gampaha Town", "Yakkala", "Miriswatta", "Ganemulla"],
+            "Ja-Ela": ["Ja-Ela", "Kandana", "Ekala", "Raddolugama"],
+            "Katana": ["Katana", "Kochchikade", "Seeduwa"],
+            "Kelaniya": ["Kelaniya", "Kiribathgoda", "Kadawatha", "Enderamulla"],
+            "Mahara": ["Kadawatha", "Kirillawala", "Eldeniya"],
+            "Minuwangoda": ["Minuwangoda", "Udugampola", "Veyangoda"],
+            "Mirigama": ["Mirigama", "Pallewela", "Weyangoda"],
+            "Negombo": ["Negombo Town", "Katuwapitiya", "Daluwakotuwa"],
+            "Wattala": ["Wattala", "Hendala", "Mabola", "Hunupitiya"]
+        }
+    },
+    "Kalutara": {
+        "DS_Divisions": {
+            "Agalawatta": ["Agalawatta", "Pimbura"],
+            "Bandaragama": ["Bandaragama", "Millaniya", "Geluwa"],
+            "Beruwala": ["Beruwala", "Aluthgama", "Darga Town", "Bentota"],
+            "Bulathsinhala": ["Bulathsinhala", "Govinna"],
+            "Dodangoda": ["Dodangoda", "Nehinna"],
+            "Horana": ["Horana Town", "Anguruwatota", "Pokunuwita"],
+            "Ingiriya": ["Ingiriya", "Handapangoda"],
+            "Kalutara": ["Kalutara Town", "Katukurunda", "Wadduwa"],
+            "Madurawela": ["Madurawela", "Anguruwatota"],
+            "Mathugama": ["Mathugama", "Welipenna"],
+            "Millaniya": ["Millaniya", "Paragastota"],
+            "Palindanuwara": ["Baduraliya", "Lathpandura"],
+            "Panadura": ["Panadura Town", "Wadduwa", "Lyceum Area", "Nalluruwa"],
+            "Walallawita": ["Walallawita", "Meegahatenna"]
+        }
+    },
+    "Kandy": {
+        "DS_Divisions": {
+            "Akurana": ["Akurana", "Pangollamada"],
+            "Delthota": ["Delthota", "Galaha"],
+            "Doluwa": ["Doluwa", "Gampola"],
+            "Ganga Ihala Korale": ["Gampola Town", "Ulapane"],
+            "Harispattuwa": ["Katugastota", "Ranawana"],
+            "Hatharaliyadda": ["Hatharaliyadda", "Galagedera"],
+            "Kandy Four Gravets": ["Kandy City", "Peradeniya", "Lewella", "Primrose"],
+            "Kundasale": ["Kundasale", "Digana", "Nattarampotha"],
+            "Medadumbara": ["Teldeniya", "Rangala"],
+            "Minipe": ["Hasalaka", "Kolongoda"],
+            "Panavila": ["Panavila", "Madulkele"],
+            "Pasbage Korale": ["Nawalapitiya Town", "Ginigathena Road"],
+            "Pathadumbara": ["Katugastota", "Madawala"],
+            "Pathahewaheta": ["Thalathuoya", "Marassana"],
+            "Poojapitiya": ["Poojapitiya", "Hedeniya"],
+            "Thumpane": ["Galagedera", "Hatharaliyadda"],
+            "Uda Palatha": ["Gampola Town", "Gelioya"],
+            "Uda Dumbara": ["Udadumbara", "Hunnasgiriya"],
+            "Udunuwara": ["Pilimathalawa", "Gelioya", "Handessa"],
+            "Yatinuwara": ["Peradeniya", "Kadugannawa", "Pilimathalawa"]
+        }
+    },
+    "Matale": {
+        "DS_Divisions": {
+            "Dambulla": ["Dambulla Town", "Sigiriya", "Inamaluwa"],
+            "Galewela": ["Galewela", "Dewahuwa"],
+            "Laggala-Pallegama": ["Pallegama", "Illukkumbura"],
+            "Matale": ["Matale Town", "Aluvihare", "Kawatayamuna"],
+            "Naula": ["Naula", "Kongahawela"],
+            "Pallepola": ["Pallepola", "Monarawila"],
+            "Rattota": ["Rattota Town", "Riverston Area"],
+            "Ukuwela": ["Ukuwela", "Elkaduwa"],
+            "Wilgamuwa": ["Wilgamuwa", "Hettipola"],
+            "Yatawatta": ["Yatawatta", "Weeragoda"]
+        }
+    },
+    "Nuwara Eliya": {
+        "DS_Divisions": {
+            "Ambagamuwa": ["Hatton Town", "Dickoya", "Maskeliya", "Nallathanniya (Sripada)"],
+            "Kotmale": ["Kotmale", "Pundaluoya", "Tawalamtenne"],
+            "Nuwara Eliya": ["Nuwara Eliya Town", "Nanu Oya", "Hakgala", "Talawakele"],
+            "Hanguranketha": ["Hanguranketha", "Rikillagaskada"],
+            "Walapane": ["Walapane", "Nildandahinna"]
+        }
+    },
+    "Galle": {
+        "DS_Divisions": {
+            "Akmeemana": ["Akmeemana", "Karapitiya", "Walahanduwa"],
+            "Ambalangoda": ["Ambalangoda Town", "Kahawe", "Randombe"],
+            "Baddegama": ["Baddegama", "Sandarawala"],
+            "Balapitiya": ["Balapitiya Town", "Ahungalla"],
+            "Bope-Poddala": ["Poddala", "Labuduwa"],
+            "Elpitiya": ["Elpitiya Town", "Aviththawa"],
+            "Galle Four Gravets": ["Galle Fort", "Galle Town", "Unawatuna", "Kaluwella"],
+            "Habaraduwa": ["Habaraduwa", "Ahangama", "Koggala"],
+            "Hikkaduwa": ["Hikkaduwa Town", "Dodanduwa", "Rathgama"],
+            "Imaduwa": ["Imaduwa", "Angulugaha"],
+            "Karandeniya": ["Karandeniya", "Urgasmanhandiya"],
+            "Neluwa": ["Neluwa", "Tawalama"],
+            "Niyagama": ["Niyagama", "Thalgaswela"],
+            "Thawalama": ["Thawalama", "Hiniduma"],
+            "Welivitiya-Divithura": ["Welivitiya", "Divithura"],
+            "Yakkalamulla": ["Yakkalamulla", "Imaduwa"]
+        }
+    },
+    "Matara": {
+        "DS_Divisions": {
+            "Akuressa": ["Akuressa Town", "Athuraliya"],
+            "Athuraliya": ["Athuraliya", "Tibbotuwawa"],
+            "Devinuwara": ["Dondra", "Devinuwara Town", "Talalla"],
+            "Dickwella": ["Dickwella Town", "Nilwella"],
+            "Hakmana": ["Hakmana", "Gangodagama"],
+            "Kamburupitiya": ["Kamburupitiya Town", "Mapalana"],
+            "Kirinda-Puhulwella": ["Puhulwella", "Kirinda"],
+            "Kotapola": ["Deniyaya Town", "Kotapola"],
+            "Malimbada": ["Malimbada", "Telijjawila"],
+            "Matara Four Gravets": ["Matara Town", "Polhena", "Meddawatta", "Karapitiya Road"],
+            "Mulatiyana": ["Mulatiyana", "Deegala"],
+            "Pasgoda": ["Pasgoda", "Urubokka"],
+            "Pitabeddara": ["Pitabeddara", "Morawaka"],
+            "Thihagoda": ["Thihagoda", "Yatiyana"],
+            "Weligama": ["Weligama Town", "Mirissa", "Midigama"],
+            "Welipitiya": ["Welipitiya", "Kananke"]
+        }
+    },
+    "Hambantota": {
+        "DS_Divisions": {
+            "Ambalantota": ["Ambalantota Town", "Ridiyagama"],
+            "Angunakolapelessa": ["Angunakolapelessa Town", "Ranna Road"],
+            "Beliatta": ["Beliatta Town", "Getamanna"],
+            "Hambantota": ["Hambantota Port Area", "Mirijjawila", "Siribopura"],
+            "Katuwana": ["Katuwana Town", "Middeniya"],
+            "Lunugamvehera": ["Lunugamvehera", "Tissamaharama Road"],
+            "Okewela": ["Okewela", "Tangalle Road"],
+            "Suriyawewa": ["Suriyawewa Town", "Bedigama"],
+            "Tangalle": ["Tangalle Town", "Ranna", "Mawella"],
+            "Tissamaharama": ["Tissamaharama Town", "Kataragama Road", "Yala Entrance"],
+            "Walasmulla": ["Walasmulla Town", "Weeraketiya"]
+        }
+    },
+    "Jaffna": {
+        "DS_Divisions": {
+            "Delft": ["Delft Island"],
+            "Island South": ["Kayts", "Mandaitivu"],
+            "Island North": ["Karainagar"],
+            "Jaffna": ["Jaffna Town", "Chundikuli", "Nallur"],
+            "Karaveddy": ["Karaveddy", "Nelliyady"],
+            "Kopay": ["Kopay", "Neervely", "Urumpirai"],
+            "Maruthankerney": ["Maruthankerney", "Chundikulam"],
+            "Nallur": ["Nallur", "Kokuvil", "Kondavil"],
+            "Point Pedro": ["Point Pedro Town", "Valvettithurai"],
+            "Sandilipay": ["Sandilipay", "Manipay"],
+            "Chavakachcheri": ["Chavakachcheri Town", "Kodikamam", "Mirusuvil"],
+            "Uduvil": ["Chunnakam Town", "Uduvil", "Kandarodai"],
+            "Valikamam East": ["Kopay", "Puttur"]
+        }
+    },
+    "Kilinochchi": {
+        "DS_Divisions": {
+            "Karachchi": ["Kilinochchi Town", "Ariviyal Nagar"],
+            "Pachchilaipalli": ["Pallai Town"],
+            "Poonakary": ["Pooneryn Town"],
+            "Palai": ["Palai Town"]
+        }
+    },
+    "Mannar": {
+        "DS_Divisions": {
+            "Mannar": ["Mannar Town", "Talaimannar", "Pesalai"],
+            "Madhu": ["Madhu Shrine Area"],
+            "Nanattan": ["Nanattan Town"],
+            "Musali": ["Silavatturai"]
+        }
+    },
+    "Vavuniya": {
+        "DS_Divisions": {
+            "Vavuniya": ["Vavuniya Town", "Nelukkulam"],
+            "Vavuniya South": ["Cheddikulam Town"],
+            "Vavuniya North": ["Nedunkeni Town"]
+        }
+    },
+    "Mullaitivu": {
+        "DS_Divisions": {
+            "Maritimepattu": ["Mullaitivu Town", "Wattappalai"],
+            "Puthukudiyiruppu": ["Puthukudiyiruppu Town"],
+            "Oddusuddan": ["Oddusuddan Town"]
+        }
+    },
+    "Trincomalee": {
+        "DS_Divisions": {
+            "Trincomalee": ["Trincomalee Town", "Nilaveli", "China Bay"],
+            "Kuchchaveli": ["Kuchchaveli", "Nilaveli Beach Area"],
+            "Kantalai": ["Kantalai Town"],
+            "Kinniya": ["Kinniya Town"],
+            "Mutur": ["Mutur Town"]
+        }
+    },
+    "Batticaloa": {
+        "DS_Divisions": {
+            "Batticaloa": ["Batticaloa Town", "Kallady"],
+            "Kalkudah": ["Valachchenai Town", "Kalkudah Beach", "Pasikudah"],
+            "Kaluwanchikudy": ["Kaluwanchikudy Town"],
+            "Eravur Pattu": ["Eravur Town"]
+        }
+    },
+    "Ampara": {
+        "DS_Divisions": {
+            "Ampara": ["Ampara Town"],
+            "Addalaichenai": ["Addalaichenai Town"],
+            "Arugam Bay": ["Pottuvil", "Arugam Bay Surf Area"],
+            "Kalmunai": ["Kalmunai Town"],
+            "Samanthurai": ["Samanthurai Town"],
+            "Pottuvil": ["Pottuvil Town", "Arugam Bay"]
+        }
+    },
+    "Kurunegala": {
+        "DS_Divisions": {
+            "Alawwa": ["Alawwa Town", "Boyawalana"],
+            "Bingiriya": ["Bingiriya Town"],
+            "Ibbagamuwa": ["Ibbagamuwa Town", "Melsiripura"],
+            "Kuliyapitiya": ["Kuliyapitiya Town", "Dandagamuwa"],
+            "Kurunegala": ["Kurunegala Town", "Malkaduwawa", "Maspotha"],
+            "Mawathagama": ["Mawathagama Town", "Mallawapitiya"],
+            "Narammala": ["Narammala Town", "Dambadeniya"],
+            "Pannala": ["Pannala Town", "Dankotuwa Road"],
+            "Polgahawela": ["Polgahawela Town"],
+            "Wariyapola": ["Wariyapola Town", "Padeniya"]
+        }
+    },
+    "Puttalam": {
+        "DS_Divisions": {
+            "Anamaduwa": ["Anamaduwa Town"],
+            "Chilaw": ["Chilaw Town", "Marawila Road"],
+            "Dankotuwa": ["Dankotuwa Town"],
+            "Kalpitiya": ["Kalpitiya Town", "Narakkalliya", "Talawila"],
+            "Madampe": ["Madampe Town"],
+            "Nattandiya": ["Nattandiya Town", "Marawila"],
+            "Puttalam": ["Puttalam Town", "Palaviya"],
+            "Wennappuwa": ["Wennappuwa Town", "Lunuwila"]
+        }
+    },
+    "Anuradhapura": {
+        "DS_Divisions": {
+            "Anuradhapura": ["Anuradhapura Sacred City", "New Town", "Pandulagama"],
+            "Eppawala": ["Eppawala Town"],
+            "Galenbindunuwewa": ["Galenbindunuwewa Town"],
+            "Kekirawa": ["Kekirawa Town", "Kalawewa"],
+            "Mihintale": ["Mihintale Town"],
+            "Nochchiyagama": ["Nochchiyagama Town"],
+            "Padaviya": ["Padaviya Town"],
+            "Talawa": ["Talawa Town"]
+        }
+    },
+    "Polonnaruwa": {
+        "DS_Divisions": {
+            "Polonnaruwa": ["Polonnaruwa Town", "Sacred City", "Giritale"],
+            "Hingurakgoda": ["Hingurakgoda Town", "Minneriya"],
+            "Medirigiriya": ["Medirigiriya Town"],
+            "Thamankaduwa": ["Kaduruwela Town", "Polonnaruwa Town"]
+        }
+    },
+    "Badulla": {
+        "DS_Divisions": {
+            "Badulla": ["Badulla Town", "Dunhinda Area"],
+            "Bandarawela": ["Bandarawela Town", "Diyatalawa"],
+            "Ella": ["Ella Town", "Ravana Area"],
+            "Haputale": ["Haputale Town", "Diyatalawa"],
+            "Mahiyanganaya": ["Mahiyanganaya Town", "Sabaragamuwa Boundary"],
+            "Welimada": ["Welimada Town", "Boralanda"]
+        }
+    },
+    "Moneragala": {
+        "DS_Divisions": {
+            "Bibile": ["Bibile Town"],
+            "Buttala": ["Buttala Town"],
+            "Kataragama": ["Kataragama Sacred Town", "Sella Kataragama"],
+            "Moneragala": ["Moneragala Town", "Wellawaya Road"],
+            "Wellawaya": ["Wellawaya Town", "Ella Road"]
+        }
+    },
+    "Ratnapura": {
+        "DS_Divisions": {
+            "Balangoda": ["Balangoda Town", "Pambahinna (Sabaragamuwa Uni)"],
+            "Embilipitiya": ["Embilipitiya Town", "Chandrika Wewa Area"],
+            "Kuruwita": ["Kuruwita Town", "Bopath Ella Area"],
+            "Pelmadulla": ["Pelmadulla Town"],
+            "Ratnapura": ["Ratnapura Town", "New Town", "Gilimale"]
+        }
+    },
+    "Kegalle": {
+        "DS_Divisions": {
+            "Galigamuwa": ["Galigamuwa Town"],
+            "Kegalle": ["Kegalle Town", "Karandupana"],
+            "Mawanella": ["Mawanella Town", "Utuwankanda"],
+            "Rambukkana": ["Rambukkana Town", "Pinnawala"],
+            "Ruwanwella": ["Ruwanwella Town"],
+            "Warakapola": ["Warakapola Town", "Ambepussa"],
+            "Yatiyantota": ["Yatiyantota Town", "Kitulgala Surf Area"]
+        }
+    }
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { SL_LOCATIONS };
+}
